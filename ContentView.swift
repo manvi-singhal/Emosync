9bc61e5eb8c343bc317842f353d8ba4cf29e9b23
Manvi Singhal <manvi.singhal03@gmail.com>
2024-02-19 23:17:00 +0530

    minor change

--- ContentView.swift
+++ ContentView.swift
@@ -10,7 +10,8 @@ struct ContentView: View {
                     .foregroundStyle(Color.accentColor)
                     .padding()
                 
-                Text("EmoSync is a simple tool that keeps track of emotions. It shows that even when things get tough, there's a way to feel good about yourself.")
+                Text("EmoSync is a simple tool that keeps track of emotions. \n It shows that even when things get tough, there's a way to feel good about yourself.")
+                    .font(.title3)
                     .padding(.horizontal, 25)
                     .multilineTextAlignment(.center)
                     .padding()
